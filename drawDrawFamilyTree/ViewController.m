b1c0ab3d9d730a70dc7b681b608d9a68f6eaebd9
Nicole <Nicole_wing@Nicole-MacBook-Pro.local>
2018-03-16 13:01:34 +0800

    first commit

--- drawDrawFamilyTree/ViewController.m
+++ drawDrawFamilyTree/ViewController.m
@@ -7,18 +7,35 @@
 //
 
 #import "ViewController.h"
+#import "FamilyModel.h"
+#import "FamilyTreeView.h"
+#import "ExampleDataTool.h"
 
-@interface ViewController ()
-
+@interface ViewController () <FamilyTreeViewDelegate,UIAlertViewDelegate>
+@property (nonatomic, strong) FamilyTreeView *familyTreeView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.title = @"家族树绘制";
+    
+    self.familyTreeView = [[FamilyTreeView alloc] initWithFrame:self.view.frame];
+    
+    FamilyModel *model = [ExampleDataTool getFamilyModelFromLocalExampleData];
+    self.familyTreeView.model = model;
+    self.familyTreeView.delegate = self;
+    [self.view addSubview:self.familyTreeView];
 }
 
+-(void)familyTreeButtonDidClickedItemAtIndex:(FamilyModel *)model{
+    NSString *titleStr = [NSString stringWithFormat:@"点击了 '%@' 按钮", model.appellation];
+    NSString *messageStr = [NSString stringWithFormat:@"节点ID：%@ && 姓名：%@", model.personId, model.name];
+    UIAlertView * alertView = [[UIAlertView alloc] initWithTitle:titleStr message:messageStr delegate:self cancelButtonTitle:@"确定" otherButtonTitles:nil];
+    [alertView show];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
